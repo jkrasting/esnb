b2de601799b0a27a4531a850f62838de598a95f4
John Krasting <John.Krasting@noaa.gov>
2025-06-26 16:47:40 -0400

    Added sphinx docs skeleton

---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ESNB'
+copyright = '2025, NOAA Model Diagnostics Task Force Team'
+author = 'NOAA Model Diagnostics Task Force Team'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+
+#-- Build api
+from sphinx.ext.apidoc import main
+main(['-f', '-M', '-e', '-T', '../../src/esnb', '-o', 'api' ])
---
+++ docs/source/index.rst
@@ -0,0 +1,15 @@
+ESNB documentation
+==================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+   :caption: API Documentation
+
+   api
+
